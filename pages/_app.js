b2024ec4915fab15010186402ecb2ab0a50b60f5
Amir Kabiri <akabiridev@gmail.com>
2021-09-22 11:07:48 +0330

    progress bar added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNProgress
+        color="#ea580c"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
